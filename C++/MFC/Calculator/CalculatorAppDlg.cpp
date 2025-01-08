b625239e8dcea9ddcb2851020a03bbdd72097afb
WHXHW <35909740+youjewon@users.noreply.github.com>
2025-01-08 21:41:13 +0900

    Calculator

---
+++ C++/MFC/Calculator/CalculatorAppDlg.cpp
@@ -0,0 +1,201 @@
+﻿
+// CalculatorAppDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "CalculatorApp.h"
+#include "CalculatorAppDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CCalculatorAppDlg 대화 상자
+
+
+
+CCalculatorAppDlg::CCalculatorAppDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CALCULATORAPP_DIALOG, pParent)
+	, m_Num1(0)
+	, m_Num2(0)
+	, m_Result(_T(""))
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CCalculatorAppDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1_NUM1, m_Num1);
+	DDX_Text(pDX, IDC_EDIT2_NUM2, m_Num2);
+	DDX_Text(pDX, IDC_STATIC_RESULT, m_Result);
+}
+
+BEGIN_MESSAGE_MAP(CCalculatorAppDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1_ADD, &CCalculatorAppDlg::OnBnClickedButton1Add)
+	ON_BN_CLICKED(IDC_BUTTON2_SUB, &CCalculatorAppDlg::OnBnClickedButton2Sub)
+	ON_BN_CLICKED(IDC_BUTTON3_MULTI, &CCalculatorAppDlg::OnBnClickedButton3Multi)
+	ON_BN_CLICKED(IDC_BUTTON4_DIV, &CCalculatorAppDlg::OnBnClickedButton4Div)
+END_MESSAGE_MAP()
+
+
+// CCalculatorAppDlg 메시지 처리기
+
+BOOL CCalculatorAppDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CCalculatorAppDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CCalculatorAppDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CCalculatorAppDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CCalculatorAppDlg::OnBnClickedButton1Add()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	m_Result.Format(_T("%d"), m_Num1 + m_Num2);
+	UpdateData(FALSE);
+}
+
+
+void CCalculatorAppDlg::OnBnClickedButton2Sub()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	m_Result.Format(_T("%d"), m_Num1 - m_Num2);
+	UpdateData(FALSE);
+}
+
+
+void CCalculatorAppDlg::OnBnClickedButton3Multi()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	m_Result.Format(_T("%d"), m_Num1 * m_Num2);
+	UpdateData(FALSE);
+}
+
+
+void CCalculatorAppDlg::OnBnClickedButton4Div()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	m_Result.Format(_T("%d"), m_Num1 / m_Num2);
+	UpdateData(FALSE);
+}
